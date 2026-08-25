67dde870b77d08866319bd72cb2d62191dd109b1
Amitoj Singh Chawla <amitojsinghkhalsa15@gmail.com>
2026-08-25 15:23:06 +0530

    ran example of subgraph with seperate state

---
+++ MCP_RAG_BOT/rag_tool.py
@@ -0,0 +1,7 @@
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.tools import tool
+
+

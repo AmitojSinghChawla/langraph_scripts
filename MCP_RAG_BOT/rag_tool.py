1ac0d7fc04038c49e6585a05511302b8554a879b
Amitoj Singh Chawla <amitojsinghkhalsa15@gmail.com>
2026-08-28 23:25:53 +0530

    nothing much just tutorial follow through memory

--- MCP_RAG_BOT/rag_tool.py
+++ MCP_RAG_BOT/rag_tool.py
@@ -1,7 +1,19 @@
-from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
-from langchain_core.tools import tool
+from unstructured.partition.pdf import partition_pdf
 
+filepath= r'/MCP_RAG_BOT/ProvisionalDegree.pdf'
 
+def create_chunks_from_pdf(file_path):
+
+    elements = partition_pdf(
+        filename=file_path,
+        strategy="fast",
+        infer_table_structure=True,
+    )
+    # → elements is a flat list of Table and CompositeElement objects
+    #   passed to table_text_segregation() and get_images() in ingest_pdfs()
+    return elements
+
+elements = create_chunks_from_pdf(filepath)
+
+for el in elements:
+    print(el.text)
